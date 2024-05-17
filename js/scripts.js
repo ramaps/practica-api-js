5e97892f1500c0a1e5ad09a243aff24fe8e80acf
Rama_PS <110693652+ramaps@users.noreply.github.com>
2024-05-17 00:11:55 -0300

    cargamos archivos
    se cargan los archivos de practica api pokemon

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+/*
+Práctica 1: (No es obligatoria) consumir una API pública y agregarle estilo para que 
+tengamos como resultado una página función, se puede agregar botón, eventos, etc.
+Se puede utilizar como base lo visto en clase.
+
+
+Práctica 2: hay que copiar el login a continuación y se puede utilizar como adelanto 
+para el trabajo práctico de JS que se habilitará a partir del 20-5.
+*/
+
+    async function getPokemonDatos() {
+        const randomNumber = Math.random() * 1302;
+        const roundedNumber = Math.floor(randomNumber);
+      try {
+        const respueta = await fetch(`https://pokeapi.co/api/v2/pokemon/${roundedNumber}`);
+        const pokemonDatos = await respueta.json();
+        console.log(pokemonDatos);
+
+    // con esto tenemos la imagen del pokemon
+    const imagenUrl = pokemonDatos.sprites.front_default;
+    console.log(imagenUrl);
+
+    // asi mostramos la imagen y los datos
+    document.getElementById('imagen').innerHTML = `<div class="card"> <img  src="${imagenUrl}" alt="${pokemonDatos.name.toUpperCase()}"><p>${pokemonDatos.name.toUpperCase()}</p> </div>`;
+
+      } catch (error) {
+        console.error(error);
+      }
+    }
